726ec8282fba83b35f9e3f288e4d702b89aa4b5d
helen-sinica <helen.sinica@gmail.com>
2015-05-27 15:37:28 +0800

    generate district_pay_raw (kNTD) and later on trim to required column (10kNTD).

--- district_pay_income.parse.R
+++ district_pay_income.parse.R
@@ -4,7 +4,7 @@ library(stringr)
 # set working directory according to the OS
 is.windows <- function() .Platform$OS.type=="windows"
 ifelse(is.windows(), setwd("D:/E-sun"), setwd('/home/helen/esbk'))
-# getwd()
+#xxx getwd()
 
 # read 22 htmls (loop by names)
 keywd <- LETTERS[-c(12, 18, 19, 25)]
@@ -44,14 +44,26 @@ for(c in keywd){
     # set column names of the matrix
     dimnames(tmp)[[2]] <- c("cty", "dist", "vil", "fam.num", "t.pay", "vil.pay", 
                             "med.pay", "Q1.pay", "Q3.pay", "sd.pay", "var.pay")
-    
+    # re-order the columns
     tmp <- tmp[,c(1:3, 6, 4:5, 7:11)]
     
+    # change column class
+    for(cl in 4:11){
+        tmp[,cl] <- as.numeric(tmp[,cl])
+    }
+    
+    # save the data to one big table
+    # P.S. *.pay: unit = 10k NTD
     if(count==1){
-        write.csv(tmp, "district_pay.csv", row.names=F)
+        write.csv(tmp, "district_pay_raw.csv", row.names=F)
     }else{
-        write.table(tmp, "district_pay.csv", row.names=F, sep=",", col.names=F, append=T)
+        write.table(tmp, "district_pay_raw.csv", row.names=F, sep=",", col.names=F, append=T)
     }
     count <- count + 1
 #是否必須統一臺或台?
 }
+
+# x <- fread("district_pay_raw.csv", data.table=F)
+# x <- x[,1:4]
+# x[,4] <- x[,4]/10
+# write.csv(x, "district_pay.csv", row.names=F)
